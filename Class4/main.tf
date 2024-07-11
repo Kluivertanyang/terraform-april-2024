02eefb69aa1627179b62df953ce85bd88d828358
Kluivertanyang <kluivertanyang@gmail.com>
2024-07-11 01:55:59 +0000

    class5 work

--- Class4/main.tf
+++ Class4/main.tf
@@ -5,7 +5,10 @@ provider aws {
 resource "aws_instance" "web" {
   ami           = var.ami_id
   instance_type = var.instance_type
+  availability_zone = var.kaizen
 }
+
+variable kaizen {}
 
 variable ami_id {
   description = "Provide ami id"
